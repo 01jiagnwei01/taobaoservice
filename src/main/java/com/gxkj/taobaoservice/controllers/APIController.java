6faf9d7892754c3977b78ef0acc130f10bb61136
xubaoyong <xubaoyong@ucredit.com>
2014-11-21 19:31:14 +0800

    init

--- src/main/java/com/gxkj/taobaoservice/controllers/APIController.java
+++ src/main/java/com/gxkj/taobaoservice/controllers/APIController.java
@@ -1,18 +1,20 @@
 package com.gxkj.taobaoservice.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/api")
 public class APIController {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/say")
+	@RequestMapping(method = RequestMethod.GET, value = "/say/{userName}")
 	@ResponseBody
-	public String sayhello(String arg){
-		return String.format("hello %s", arg);
+	public String sayhello(@PathVariable(value = "userName") String userName, @RequestParam(defaultValue = "") String name){
+		return String.format("hello %s  %s", userName,name);
 	} 
 	
 	public static void main(String[] args) {
