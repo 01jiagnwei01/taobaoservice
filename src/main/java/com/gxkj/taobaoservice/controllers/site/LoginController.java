5187207db98c7e1f8500423a58218f5864d19113
xubaoyong <xubaoyong@ucredit.com>
2014-12-01 13:54:13 +0800

    避免与后台的Controller冲突

--- src/main/java/com/gxkj/taobaoservice/controllers/site/LoginController.java
+++ src/main/java/com/gxkj/taobaoservice/controllers/site/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
+@Controller(value="siteLogin")
 @RequestMapping("/login")
 public class LoginController {
 
