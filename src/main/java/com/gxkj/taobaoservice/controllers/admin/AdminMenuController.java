6042004160bc505b6f63a7510ddffd81c6c7eba0
xubaoyong <xubaoyong@ucredit.com>
2014-12-01 16:12:21 +0800

    优化

--- src/main/java/com/gxkj/taobaoservice/controllers/admin/AdminMenuController.java
+++ src/main/java/com/gxkj/taobaoservice/controllers/admin/AdminMenuController.java
@@ -7,12 +7,9 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
