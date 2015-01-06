70278f7fa87aac4c2f5b24c8f2cbf9e35bc1e139
xubaoyong <xubaoyong@ucredit.com>
2015-01-06 10:52:30 +0800

    拦截器优化

--- src/main/java/com/gxkj/taobaoservice/filters/AuthorizationInterceptor.java
+++ src/main/java/com/gxkj/taobaoservice/filters/AuthorizationInterceptor.java
@@ -1,15 +1,28 @@
 package com.gxkj.taobaoservice.filters;
 
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 //import org.springframework.web.servlet.mvc.multiaction.InternalPathMethodNameResolver;
 //import org.springframework.web.servlet.mvc.multiaction.MethodNameResolver;
+
+
+
+import org.springframework.web.servlet.mvc.multiaction.InternalPathMethodNameResolver;
+import org.springframework.web.servlet.mvc.multiaction.MethodNameResolver;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gxkj.common.annotation.WithoutAuthorize;
@@ -24,12 +37,15 @@ import com.gxkj.taobaoservice.entitys.AdminUser;
 public class AuthorizationInterceptor extends HandlerInterceptorAdapter  {
 
 	 
-	
-	public void afterCompletion(HttpServletRequest arg0,
-			HttpServletResponse arg1, Object arg2, Exception arg3)
+	/**
+	 * 当有拦截器抛出异常时,会从当前拦截器往回执行所有的拦截器的afterCompletion() 
+	 */
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
- 
-		 super.afterCompletion(arg0, arg1, arg2, arg3) ;  
+		if(ex!=null)
+		ex.printStackTrace();
+		 super.afterCompletion(request, response, handler, ex) ;  
 	}
 
 	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
@@ -39,14 +55,36 @@ public class AuthorizationInterceptor extends HandlerInterceptorAdapter  {
 	}
 
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse response,
-			Object arg2) throws Exception {
+			Object handler) throws Exception {
+		MethodNameResolver methodNameResolver = new InternalPathMethodNameResolver();
+		System.out.println("methodName="+methodNameResolver.getHandlerMethodName(req));
+		System.out.println("权限拦截---------------");
+		
+//		HandlerMethod handlerMethod = (HandlerMethod) handler;        
+//		Method method = handlerMethod.getMethod();
+		
 		String url = req.getRequestURI();
 		String ctx = req.getContextPath();
 		url = url.replace(ctx, "");
+		Enumeration<String> keys = req.getParameterNames();
+		while(keys.hasMoreElements()) {
+		    String k = keys.nextElement();
+		    System.out.println(k + " = " + req.getParameter(k) );
+		} 
+		
+		Map pathVariables = (Map) req.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		if(pathVariables!=null){
+			Set set = pathVariables.keySet();
+			Iterator it =set.iterator();
+			while(it.hasNext()){
+				System.out.println(it.next());
+			}
+		}
 		
 		
-		if(arg2 !=null  ){
-			Class<? extends Object> clazz =  arg2.getClass();
+		
+		if(handler !=null  ){
+			Class<? extends Object> clazz =  handler.getClass();
 			if(clazz.isAnnotationPresent(WithoutAuthorize.class)){
 				return true;
 			} 
