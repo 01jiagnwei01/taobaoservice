258fe6261d6d96bf42cf027a360f97140788a45b
xubaoyong <xubaoyong@paypalm.cn>
2015-02-02 10:28:54 +0800

    韩松页面提交

---
+++ src/main/groovy/mina/MinaTimeServer.groovy
@@ -0,0 +1,33 @@
+package mina
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.core.service.IoAcceptor;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor
+
+class MinaTimeServer {
+
+	private static final int PORT = 9123;
+	
+	static main(args) {
+		 
+		IoAcceptor acceptor = new NioSocketAcceptor();
+
+        acceptor.getFilterChain().addLast( "logger", new LoggingFilter() );
+        acceptor.getFilterChain().addLast( "codec", new ProtocolCodecFilter( new TextLineCodecFactory( Charset.forName( "UTF-8" ))));
+
+        acceptor.setHandler( new TimeServerHandler() );
+		acceptor.getSessionConfig().setReadBufferSize( 2048 );
+        acceptor.getSessionConfig().setIdleTime( IdleStatus.BOTH_IDLE, 10 );
+        acceptor.bind( new InetSocketAddress(PORT) );
+
+
+
+		
+	}
+
+}
